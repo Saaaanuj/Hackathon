462cb109aac98200a7753802774cc238dc5b8cb4
HellBats <vishalgoyal452003@gmail.com>
2024-02-09 22:24:26 +0530

    features

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const auth = require('./auth.js');
+const auth = require('./controller.js');
 const PORT = 3001;
-
+const path = '';
 const app = express();
+const change_requests_limit = 50;
+change_requests = 0;
 app.use(express.json());
 
 
 app.post('/signup',function(req,res){
     data = {
+        "name":req.body.name,
         "username":req.body.username,
-        "password":req.body.password
+        "password":req.body.password,
+        "hostel":req.body.hostel
     }
-    token = auth.GetToken(data);
-    res.cookie('uid',token);
-    res.json({'msg':'Done'});
+    auth.add(data)
+    res.json({'msg':'Succesfully Signed up'});
 })
 
 
@@ -22,18 +25,43 @@ app.post('/login',function(req,res){
         "username":req.body.username,
         "password":req.body.password
     }
-    token = auth.GetToken(data);
-    res.json({'msg':token})
+    if(auth.isPass(data)) {
+        res.json({'msg':"Succesfully Logged in"});
+    }
+    else res.json({"msg":"Account does not exist"})
 })
 
-
-app.use(auth.VerifyUserMiddleWare);
 
 app.get('/',function(req,res){
-    res.json({'msg':'Done'});
+    res.sendFile(path);
 })
 
-app.get('/cookie',function(req,res){
-    res.json({'msg':'Cookie'});
+
+app.get('/messmenu',function(req,res){
+    data = MenuData();
+    res.json(data);
 })
+
+app.post('/eatfood',function(req,res){
+    data = {
+        "username": req.body.username,
+        "eatfood": req.body.eatfood  
+    }
+    SendData(data);
+    res.json({'msg':'succesful'});
+})
+
+app.post("/messmenu",function(req,res){
+    data = {"username":req.body.username};
+    if(auth.isUser(data))
+    {
+        change_requests++;
+        if(change_requests==change_requests_limit)
+        {
+            SendMenuRequest();
+            change_requests = 0;
+        }
+    }
+})
+
 app.listen(PORT);
