f789a4ef769713cb889902fb7cebe1a06d6eabc2
Saaaanuj <naa2.20127382@gmail.com>
2024-02-09 10:58:46 +0530

    s

--- Backend/index.js
+++ Backend/index.js
@@ -2,3 +2,4 @@ const methods = require("./controller");
 
 //functions call = methods.isUser,methods.isPass
 //returns true false
+//functions call = methods.add
--- Backend/queries.js
+++ Backend/queries.js
@@ -1,9 +1,10 @@
 const checkUser = "SELECT * FROM students WHERE username=$1";
 const checkPass = "SELECT * FROM students WHERE username=$1 AND password=$2";
-const addUser = "INSERT INTO students (name,username,password,hostel) VALUES($1,$2,$3,$4)";
+const addUser =
+	"INSERT INTO students (name,username,password,hostel) VALUES($1,$2,$3,$4)";
 
 module.exports = {
 	checkUser,
 	checkPass,
-    addUser,
+	addUser,
 };
